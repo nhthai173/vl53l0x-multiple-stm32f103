40476c8e2370019d28d5d8297827998266da7630
Thái <62926491+nhthai173@users.noreply.github.com>
2023-10-19 15:08:03 +0700

    Init Project

---
+++ Core/Src/i2c.c
@@ -0,0 +1,304 @@
+/*
+ * i2c.c
+ *
+ *  Created on: Oct 17, 2023
+ *      Author: thainguyen
+ */
+
+
+#include "i2c.h"
+// check the error flag and set the corresponding error code,
+// then clear the corresponding flag
+uint8_t i2c1_check_error(){
+	uint8_t i2c1_error = 0;
+	if (I2C1->SR1 & I2C_SR1_TIMEOUT){
+		i2c1_error |= I2C_TIMEOUT_TLOW_ERROR;
+		I2C1->SR1 &=~ I2C_SR1_TIMEOUT;
+	}
+	if (I2C1->SR1 & I2C_SR1_PECERR){
+		i2c1_error |= I2C_PEC_ERROR;
+		I2C1->SR1 &=~ I2C_SR1_PECERR;
+	}
+	if (I2C1->SR1 & I2C_SR1_OVR){
+		i2c1_error |= I2C_OVERRUN_UNDERRUN;
+		I2C1->SR1 &=~ I2C_SR1_OVR;
+	}
+	if (I2C1->SR1 & I2C_SR1_AF){
+		i2c1_error |= I2C_ACKNOWLEDGE_FAILURE;
+		I2C1->SR1 &=~ I2C_SR1_AF;
+	}
+	if (I2C1->SR1 & I2C_SR1_ARLO){
+		i2c1_error |= I2C_ARBITRATION_LOSS;
+		I2C1->SR1 &=~ I2C_SR1_ARLO;
+	}
+	if (I2C1->SR1 & I2C_SR1_BERR){
+		i2c1_error |= I2C_BUS_ERROR;
+		I2C1->SR1 &=~ I2C_SR1_BERR;
+	}
+	return i2c1_error;
+}
+
+/* initialize the I2C1 peripheral in fast mode with f_SCL=360kHz assuming an APB1/PCLK1 clock of 36MHz
+   note: 400kHz can only be achieved when PCLK1 is a multiple of 10MHz */
+void init_i2c1(){
+	// enable alternate function and port B I/O peripheral clock
+	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN | RCC_APB2ENR_IOPBEN;
+	// enable the peripheral clock for the I2C1 module
+	RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
+#if I2C1_REMAP
+	// set pins PB8 & PB9 as alternate function open-drain output with max speed of 50MHz (REMAP)
+	GPIOB->CRH &=~ ( GPIO_CRH_MODE8 | GPIO_CRH_MODE9 | GPIO_CRH_CNF8 | GPIO_CRH_CNF9 );
+	GPIOB->CRH |= 	GPIO_CRH_MODE8_1 | GPIO_CRH_MODE8_0 |GPIO_CRH_CNF8_0 | GPIO_CRH_CNF8_1 | GPIO_CRH_MODE9_1 | GPIO_CRH_MODE9_0 |GPIO_CRH_CNF9_0 | GPIO_CRH_CNF9_1 ;
+	AFIO->MAPR |= AFIO_MAPR_I2C1_REMAP;
+#else
+	// set pins PB6 & PB7 as alternate function open-drain output with max speed of 50MHz
+	GPIOB->CRL &=~ ( GPIO_CRL_MODE6 | GPIO_CRL_MODE7 | GPIO_CRL_CNF6 | GPIO_CRL_CNF7 );
+	GPIOB->CRL |= 	GPIO_CRL_MODE6_1 | GPIO_CRL_MODE6_0 | GPIO_CRL_CNF6_0 | GPIO_CRL_CNF6_1 | GPIO_CRL_MODE7_1 | GPIO_CRL_MODE7_0 |GPIO_CRL_CNF7_0 | GPIO_CRL_CNF7_1 ;
+#endif
+	// reset the I2C1 peripheral
+	RCC->APB1RSTR |= RCC_APB1RSTR_I2C1RST;
+	RCC->APB1RSTR &=~RCC_APB1RSTR_I2C1RST;
+	// set the APB1 clock value so the I2C peripheral can derive the correct timings
+	// APB1 clock is 36MHz since SysCoreClock==72E6 and RCC_CFGR_PPRE1_DIV2==1
+	I2C1->CR2 = (36 & I2C_CR2_FREQ);
+#if I2C1_FAST_MODE
+	// set I2C master mode to fast mode with DUTY=1 and set the CCR value
+	// CCR = PCLK1[Hz] / (25*400e3[Hz]), so here CCR=36e6/(25*400e3)=3.6
+	// so we have to round to the next larger value (i.e. 4) and thus can only achieve a
+	// frequency of f = 360kHz (to achieve 400kHz, PCLK1 must be a multiple of 10MHz)
+	I2C1->CCR = I2C_CCR_FS | I2C_CCR_DUTY | 4;
+	// set the TRISE value, it is calculated as follows for fast mode: Trise = 1 + 300E-9*PCLK1[Hz]
+	// so here Trise=1+300E-9*36E6=11.8, round down to 11
+	I2C1->TRISE = 11;
+#else
+	// set I2C master mode to standard mode (100kHz) with DUTY= 0 and set the CCR value
+	// CCR = PCLK1[Hz] / (2*100e3[Hz]), so here CCR=36e6/(2*100e3)=180
+	I2C1->CCR = 180;
+	// set the TRISE value, it is calculated as follows for standard mode: Trise = 1 + 1E-6*PCLK1[Hz]
+	// so here Trise=1+36=37 (round down to nearest integer in case of fraction)
+	I2C1->TRISE = 37;
+#endif
+	//enable the I2C1 peripheral
+	I2C1->CR1 |= I2C_CR1_PE;
+}
+
+
+/*	read N bytes from the I2C slave
+	returns 0 if no error occured
+	returns an error code > 0 if an error occured	*/
+uint8_t i2c_read( uint8_t slave_address, uint8_t* data, uint8_t N ){
+	uint8_t i2c1_error  = 0;
+	// send START condition
+	I2C1->CR1 |= I2C_CR1_START;
+	// wait until START has been sent
+	// check for an error to prevent getting stuck in the loop
+	uint32_t t1 = HAL_GetTick();
+	while( !(I2C1->SR1 & I2C_SR1_SB) ){
+		i2c1_error = i2c1_check_error();
+		if( (HAL_GetTick() - t1) > 10 ) i2c1_error |= I2C_TIMEOUT_GENERAL;
+		if( i2c1_error > 0) goto stop;
+	}
+	// send slave address + R/W bit (1 for read)
+	I2C1->DR = (slave_address << 1) | 1;
+	// wait until slave address has been sent
+	// check for an error to prevent getting stuck in the loop
+	t1 = HAL_GetTick();
+	while( !(I2C1->SR1 & I2C_SR1_ADDR) ){
+		i2c1_error = i2c1_check_error();
+		if( (HAL_GetTick() - t1) > 10 ) i2c1_error |= I2C_TIMEOUT_GENERAL;
+		if( i2c1_error > 0) goto stop;
+	}
+	if (N==1){
+		// no acknowledge returned
+		I2C1->CR1 &=~I2C_CR1_ACK;
+		__disable_irq();
+		// dummy readout of the SR1 and SR2 registers to clear the ADDR flag
+		I2C1->SR1;
+		I2C1->SR2;
+		// generate STOP after the current byte transfer
+		I2C1->CR1 |= I2C_CR1_STOP;
+		__enable_irq();
+		// wait until data receive register not empty
+		// check for an error to prevent getting stuck in the loop
+		t1 = HAL_GetTick();
+		while( !(I2C1->SR1 & I2C_SR1_RXNE) ){
+			i2c1_error = i2c1_check_error();
+			if( (HAL_GetTick() - t1) > 10 ) i2c1_error |= I2C_TIMEOUT_GENERAL;
+			if( i2c1_error > 0) goto stop;
+		}
+		// read the data byte
+		*data = I2C1->DR;
+		// wait until the STOP condition has been sent
+		// check for an error to prevent getting stuck in the loop
+		t1 = HAL_GetTick();
+		while( I2C1->CR1 & I2C_CR1_STOP){
+			i2c1_error = i2c1_check_error();
+			if( (HAL_GetTick() - t1) > 10 ) i2c1_error |= I2C_TIMEOUT_GENERAL;
+			if( i2c1_error > 0) goto stop;
+		}
+		// acknowledge returned (to be ready for another reception)
+		I2C1->CR1 |= I2C_CR1_ACK;
+	}else if(N==2){
+		// ACK bit controls the (N)ACK of the next byte which will be received in the shift register
+		I2C1->CR1 |= I2C_CR1_POS;
+		__disable_irq();
+		// dummy readout of the SR1 and SR2 registers to clear the ADDR flag
+		I2C1->SR1;
+		I2C1->SR2;
+		// no acknowledge returned
+		I2C1->CR1 &=~I2C_CR1_ACK;
+		__enable_irq();
+		// wait until a new byte is received (including ACK pulse) and DR has not been read yet
+		// check for an error to prevent getting stuck in the loop
+		t1 = HAL_GetTick();
+		while( !(I2C1->SR1 & I2C_SR1_BTF) ){
+			i2c1_error = i2c1_check_error();
+			if( (HAL_GetTick() - t1) > 10 ) i2c1_error |= I2C_TIMEOUT_GENERAL;
+			if( i2c1_error > 0) goto stop;
+		}
+		__disable_irq();
+		// generate STOP after the current byte transfer
+		I2C1->CR1 |= I2C_CR1_STOP;
+		// read the first byte
+		*data = I2C1->DR;
+		__enable_irq();
+		// and the second byte
+		++data;
+		*data = I2C1->DR;
+		// wait until the STOP condition has been sent
+		// check for an error to prevent getting stuck in the loop
+		t1 = HAL_GetTick();
+		while( I2C1->CR1 & I2C_CR1_STOP){
+			i2c1_error = i2c1_check_error();
+			if( (HAL_GetTick() - t1) > 10 ) i2c1_error |= I2C_TIMEOUT_GENERAL;
+			if( i2c1_error > 0) goto stop;
+		}
+		// ACK bit controls the (N)ACK of the current byte being received in the shift register (default)
+		I2C1->CR1 &=~I2C_CR1_POS;
+		// acknowledge returned (to be ready for another reception)
+		I2C1->CR1 |= I2C_CR1_ACK;
+	}else if(N>2){
+		// dummy readout of the SR1 and SR2 registers to clear the ADDR flag
+		I2C1->SR1;
+		I2C1->SR2;
+		while(N>=3){
+			// wait until a new byte is received (including ACK pulse) and DR has not been read yet
+			// check for an error to prevent getting stuck in the loop
+			t1 = HAL_GetTick();
+			while( !(I2C1->SR1 & I2C_SR1_BTF) ){
+				i2c1_error = i2c1_check_error();
+				if( (HAL_GetTick() - t1) > 10 ) i2c1_error |= I2C_TIMEOUT_GENERAL;
+				if( i2c1_error > 0) goto stop;
+			}
+			// read one byte
+			*data = I2C1->DR;
+			++data;
+			// decrement number of bytes to read
+			--N;
+		}
+		// wait until a new byte is received (including ACK pulse) and DR has not been read yet
+		// check for an error to prevent getting stuck in the loop
+		t1 = HAL_GetTick();
+		while( !(I2C1->SR1 & I2C_SR1_BTF) ){
+			i2c1_error = i2c1_check_error();
+			if( (HAL_GetTick() - t1) > 10 ) i2c1_error |= I2C_TIMEOUT_GENERAL;
+			if( i2c1_error > 0) goto stop;
+		}
+		// no acknowledge returned
+		I2C1->CR1 &=~I2C_CR1_ACK;
+		__disable_irq();
+		// generate STOP after the current byte transfer
+		I2C1->CR1 |= I2C_CR1_STOP;
+		// read the penultimate byte
+		*data = I2C1->DR;
+		++data;
+		__enable_irq();
+		// wait until data receive register not empty
+		// check for an error to prevent getting stuck in the loop
+		t1 = HAL_GetTick();
+		while( !(I2C1->SR1 & I2C_SR1_RXNE) ){
+			i2c1_error = i2c1_check_error();
+			if( (HAL_GetTick() - t1) > 10 ) i2c1_error |= I2C_TIMEOUT_GENERAL;
+			if( i2c1_error > 0) goto stop;
+		}
+		// read the last byte
+		*data = I2C1->DR;
+		// wait until the STOP condition has been sent
+		t1 = HAL_GetTick();
+		while( I2C1->CR1 & I2C_CR1_STOP){
+			i2c1_error = i2c1_check_error();
+			if( (HAL_GetTick() - t1) > 10 ) i2c1_error |= I2C_TIMEOUT_GENERAL;
+			if( i2c1_error > 0) break;
+		}
+		// acknowledge returned (to be ready for another reception)
+		I2C1->CR1 |= I2C_CR1_ACK;
+	}
+	return 0; // no error
+stop:
+	// send STOP condition
+	I2C1->CR1 |= I2C_CR1_STOP;
+	// wait until STOP condition has been generated
+	t1 = HAL_GetTick();
+	while( I2C1->CR1 & I2C_CR1_STOP){
+		i2c1_error = i2c1_check_error();
+		if( (HAL_GetTick() - t1) > 10 ) i2c1_error |= I2C_TIMEOUT_GENERAL;
+		if( i2c1_error > 0) break;
+	}
+	return i2c1_error;
+}
+
+/*	write N bytes to the I2C slave
+	returns 0 if no error occured
+	returns an error code > 0 if an error occured	*/
+uint8_t i2c_write( uint8_t slave_address, uint8_t* data, uint8_t N ){
+	uint8_t i2c1_error  = 0;
+	// send START condition
+	I2C1->CR1 |= I2C_CR1_START;
+	// wait until START has been sent
+	// check for an error to prevent getting stuck in the loop
+	uint32_t t1 = HAL_GetTick();
+	while( !(I2C1->SR1 & I2C_SR1_SB) ){
+		i2c1_error = i2c1_check_error();
+		if( (HAL_GetTick() - t1) > 10 ) i2c1_error |= I2C_TIMEOUT_GENERAL;
+		if( i2c1_error > 0) goto stop;
+	}
+	// send slave address + R/W bit (0 for write)
+	I2C1->DR = (slave_address << 1) | 0;
+	// wait until slave address has been sent
+	// check for an error to prevent getting stuck in the loop
+	t1 = HAL_GetTick();
+	while( !(I2C1->SR1 & I2C_SR1_ADDR) ){
+		i2c1_error = i2c1_check_error();
+		if( (HAL_GetTick() - t1) > 10 ) i2c1_error |= I2C_TIMEOUT_GENERAL;
+		if( i2c1_error > 0) goto stop;
+	}
+	// dummy readout of the SR1 and SR2 registers to clear the ADDR flag
+	I2C1->SR1;
+	I2C1->SR2;
+	// send N bytes
+	while( N>0 ){
+		// write the byte to be sent into the data register
+		I2C1->DR = *data;
+		// wait until data byte transfer succeeded
+		// check for an error to prevent getting stuck in the loop
+		t1 = HAL_GetTick();
+		while( !(I2C1->SR1 & I2C_SR1_BTF) ){
+			i2c1_error = i2c1_check_error();
+			if( (HAL_GetTick() - t1) > 10 ) i2c1_error |= I2C_TIMEOUT_GENERAL;
+			if( i2c1_error > 0) goto stop;
+		}
+		++data;
+		--N;
+	}
+stop:
+	// send STOP condition
+	I2C1->CR1 |= I2C_CR1_STOP;
+	// wait until STOP condition has been generated
+	t1 = HAL_GetTick();
+	while( I2C1->CR1 & I2C_CR1_STOP){
+		i2c1_error = i2c1_check_error();
+		if( (HAL_GetTick() - t1) > 10 ) i2c1_error |= I2C_TIMEOUT_GENERAL;
+		if( i2c1_error > 0) break;
+	}
+	return i2c1_error;
+}
---
+++ Core/Src/main.c
@@ -0,0 +1,297 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+// Uncomment this line to use long range mode. This
+// increases the sensitivity of the sensor and extends its
+// potential range, but increases the likelihood of getting
+// an inaccurate reading because of reflections from objects
+// other than the intended target. It works best in dark
+// conditions.
+
+#define LONG_RANGE
+
+
+// Uncomment ONE of these two lines to get
+// - higher speed at the cost of lower accuracy OR
+// - higher accuracy at the cost of lower speed
+
+#define HIGH_SPEED
+//#define HIGH_ACCURACY
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
+/* USER CODE BEGIN PV */
+
+struct VL53L0X myTOFsensor = {
+        .hi2c = &hi2c1,
+        .io_2v8 = true,
+//        .address = 0b0101001,
+        .address = 0x29,
+        .io_timeout = 500,
+        .did_timeout = false
+};
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+
+static void MX_GPIO_Init(void);
+
+static void MX_I2C1_Init(void);
+/* USER CODE BEGIN PFP */
+
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void) {
+    /* USER CODE BEGIN 1 */
+
+    /* USER CODE END 1 */
+
+    /* MCU Configuration--------------------------------------------------------*/
+
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
+
+    /* USER CODE BEGIN Init */
+
+    /* USER CODE END Init */
+
+    /* Configure the system clock */
+    SystemClock_Config();
+
+    /* USER CODE BEGIN SysInit */
+
+    /* USER CODE END SysInit */
+
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_I2C1_Init();
+    /* USER CODE BEGIN 2 */
+
+    if( VL53L0X_init(&myTOFsensor) ){
+        // success
+    }else{
+        // error
+        return 0;
+    }
+
+
+#ifdef LONG_RANGE
+    // lower the return signal rate limit (default is 0.25 MCPS)
+	VL53L0X_setSignalRateLimit(&myTOFsensor, 0.1);
+	// increase laser pulse periods (defaults are 14 and 10 PCLKs)
+	VL53L0X_setVcselPulsePeriod(&myTOFsensor, VcselPeriodPreRange, 18);
+	VL53L0X_setVcselPulsePeriod(&myTOFsensor, VcselPeriodFinalRange, 14);
+#endif
+#ifdef HIGH_SPEED
+    // reduce timing budget to 20 ms (default is about 33 ms)
+	VL53L0X_setMeasurementTimingBudget(&myTOFsensor, 20000);
+#else //HIGH_ACCURACY
+    // increase timing budget to 200 ms
+    VL53L0X_setMeasurementTimingBudget(&myTOFsensor, 200000);
+#endif
+
+    VL53L0X_startContinuous(&myTOFsensor, 0);
+
+
+    /* USER CODE END 2 */
+
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    while (1) {
+        /* USER CODE END WHILE */
+
+        /* USER CODE BEGIN 3 */
+
+        uint16_t value = VL53L0X_readRangeContinuousMillimeters(&myTOFsensor);
+        if ( VL53L0X_timeoutOccurred(&myTOFsensor) ) {
+            // Timeout
+        }
+        HAL_Delay(500);
+
+    }
+    /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+    /** Initializes the RCC Oscillators according to the specified parameters
+    * in the RCC_OscInitTypeDef structure.
+    */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
+
+    /** Initializes the CPU, AHB and APB buses clocks
+    */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                                  | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+        Error_Handler();
+    }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void) {
+
+    /* USER CODE BEGIN I2C1_Init 0 */
+
+    /* USER CODE END I2C1_Init 0 */
+
+    /* USER CODE BEGIN I2C1_Init 1 */
+
+    /* USER CODE END I2C1_Init 1 */
+    hi2c1.Instance = I2C1;
+    hi2c1.Init.ClockSpeed = 100000;
+    hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+    hi2c1.Init.OwnAddress1 = 0;
+    hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+    hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+    hi2c1.Init.OwnAddress2 = 0;
+    hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+    hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+    if (HAL_I2C_Init(&hi2c1) != HAL_OK) {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN I2C1_Init 2 */
+
+    /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void) {
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+    /* GPIO Ports Clock Enable */
+    __HAL_RCC_GPIOD_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    /*Configure GPIO pin Output Level */
+    HAL_GPIO_WritePin(XSHUT_GPIO_Port, XSHUT_Pin, GPIO_PIN_RESET);
+
+    /*Configure GPIO pin : XSHUT_Pin */
+    GPIO_InitStruct.Pin = XSHUT_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(XSHUT_GPIO_Port, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void) {
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
+    __disable_irq();
+    while (1) {
+    }
+    /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
